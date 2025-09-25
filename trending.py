02bd5f5b493f1e35b1cc47b5ceb663437efd74c5
ndikun-inv <y.andikristianto@gmail.com>
2025-09-25 22:22:38 +0700

    trending.py

--- trending.py
+++ trending.py
@@ -14,7 +14,7 @@ query = """
 }
 """
 
-headers = {"Authorization": f"Apikey {SENTIMENT_API_KEY}"}
+headers = {"Authorization": f"Apikey {API_KEY}"}
 response = requests.post(url, json={"query": query}, headers=headers)
 
 print("RAW RESPONSE:", response.text)  # <-- DEBUG CETAK ISI RESPON
