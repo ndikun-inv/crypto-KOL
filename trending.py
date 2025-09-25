15849f403df90768b844afecf6c51f99eb12f32a
ndikun-inv <y.andikristianto@gmail.com>
2025-09-25 22:04:08 +0700

    trending.py

--- trending.py
+++ trending.py
@@ -1,19 +1,28 @@
-import requests
-from datetime import datetime, timedelta, timezone
-import csv
+import os, requests, csv, datetime
 
-API_KEY = "API_KEY"
-url = "https://api.lunarcrush.com/v2?data=market&key=" + API_KEY
+API_KEY = os.getenv("API_KEY")
+url = "https://api.santiment.net/graphql"
 
-res = requests.get(url)
+query = """
+{
+  trendingWords{
+    topWords{
+      word
+      score
+    }
+  }
+}
+"""
+
+res = requests.post(url, json={'query': query}, headers={"Authorization": f"Bearer {API_KEY}"})
 data = res.json()
 
-now = datetime.now(timezone.utc)
-wib = now + timedelta(hours=7)
+utc_now = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+wib_now = (datetime.datetime.utcnow() + datetime.timedelta(hours=7)).strftime("%Y-%m-%d %H:%M:%S")
 
 with open("trending.csv", "a", newline="") as f:
     writer = csv.writer(f)
-    for coin in data.get("data", [])[:5]:  # ambil 5 coin teratas
-        symbol = coin.get("s")
-        print(symbol, now, wib)
-        writer.writerow([symbol, now.isoformat(), wib.isoformat()])
+    for w in data["data"]["trendingWords"]["topWords"]:
+        writer.writerow([utc_now, wib_now, w["word"], w["score"]])
+
+print("Done. Saved trending.csv")
